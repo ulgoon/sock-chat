1b82619efc02c15f9d8102e44c80ed1ba12ee37e
ulgoon <ulgoon89@gmail.com>
2021-10-07 17:54:18 +0900

    feat: Static file serve (express done)
    with adding app.use('public', express.static(__dirname + "/public"))

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path');
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use("/public", express.static(__dirname + "/public"));
+
 const indexRouter = require('./routes/index');
 const clubsRouter = require('./routes/clubs');
 
