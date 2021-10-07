f9360d19a52c2b0209424c719d164b7ad55c0e6d
ulgoon <ulgoon89@gmail.com>
2021-10-07 16:18:56 +0900

    feat: Init express
    with
    
    `$ npm install --save express socket.io`
    
    To start,
    
    `$ node app.js`

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+
+app.get('/', (req, res) => {
+    res.send('Express work!');
+});
+
+server.listen(3000, () => {
+    console.log('Express Server is now listening on localhost:3000..');
+});
