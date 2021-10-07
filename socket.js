32b18e87a0218aa095336b20002b710b80608c92
ulgoon <ulgoon89@gmail.com>
2021-10-07 18:52:42 +0900

    feat: Broadcasting message ( socketio done )

--- socket.js
+++ socket.js
@@ -3,9 +3,10 @@ module.exports = (io) => {
         console.log('A user is now connected');
 
         socket.on('chat message', (msg) => {
+            io.emit('chat message', msg);
             console.log("message: ", msg);
         });
-        
+
         socket.on('disconnect', () => {
             console.log('disconnected');
         });
