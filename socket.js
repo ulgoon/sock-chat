9a0adbf5e66a119d33590d1b7a20bfb63f923c59
ulgoon <ulgoon89@gmail.com>
2021-10-07 18:34:08 +0900

    feat: Handle disconnection

--- socket.js
+++ socket.js
@@ -1,5 +1,8 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('A user is now connected');
+        socket.on('disconnect', () => {
+            console.log('disconnected');
+        });
     });
 };
