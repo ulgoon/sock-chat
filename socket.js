ad98df2c2891aaa8a2c84f24205048323958dd83
ulgoon <ulgoon89@gmail.com>
2021-10-07 18:42:34 +0900

    feat: emit message
    console.log emitted message

--- socket.js
+++ socket.js
@@ -1,6 +1,11 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('A user is now connected');
+
+        socket.on('chat message', (msg) => {
+            console.log("message: ", msg);
+        });
+        
         socket.on('disconnect', () => {
             console.log('disconnected');
         });
