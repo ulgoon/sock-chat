c5c4da9ee1b52f3be38f64e73585b16d1027cee5
ulgoon <ulgoon89@gmail.com>
2021-10-07 18:18:01 +0900

    feat: Route & Render for /chat

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ app.use("/public", express.static(__dirname + "/public"));
 
 const indexRouter = require('./routes/index');
 const clubsRouter = require('./routes/clubs');
+const chatRouter = require('./routes/chat');
 
 app.use('/', indexRouter);
 app.use('/clubs', clubsRouter);
+app.use('/chat', chatRouter);
 
 server.listen(3000, () => {
     console.log('Express Server is now listening on localhost:3000..');
